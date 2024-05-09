8f85aabd09437e7cfe63a0888f1aa8b1e6585925
Adem Kedir <ademkedir724@gmail.com>
2024-05-09 17:48:13 +0300

    remove react.svg

---
+++ src/assets/component/Navigation.jsx
@@ -0,0 +1,16 @@
+export default function Navigation() {
+  return (
+    <div className="navigation">
+      <p>
+        <span style={{ color: 'red' }}>CV c</span>reator
+      </p>
+      <div>
+        Contact me
+        <div>
+          <a href="#">🐈</a> <a href="#">🧩</a> <a href="#">🐦</a>
+          <span>Merwan KG</span>
+        </div>
+      </div>
+    </div>
+  );
+}
