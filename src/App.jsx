2aecc8d29d2a0dba685962db2e387f948059c51c
Adem Kedir <ademkedir724@gmail.com>
2024-05-09 17:48:32 +0300

    create side-bar & card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Navigation from './assets/component/Navigation';
+
 export default function App() {
   return (
     <>
@@ -11,29 +13,90 @@ export default function App() {
   );
 }
 
-function Navigation() {
+function SideBar({ children }) {
   return (
-    <div className="navigation">
-      <p>
-        <span style={{ color: 'red' }}>CV c</span>reator
-      </p>
-      <div>
-        Contact me
-        <div>
-          <a href="#">🐈</a> <a href="#">🧩</a> <a href="#">🐦</a>
-          <span>Merwan KG</span>
-        </div>
+    <div className="side-bar">
+      <div className="general-page-description">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque natus
+          quos eos dicta fugiat laboriosam aliquid amet quisquam magnam ex!
+          Officia illum velit aperiam explicabo nesciunt aspernatur ad laborum
+          perferendis
+        </p>
+      </div>
+      <div className="cards">
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+        <Card
+          tittle={'personal information'}
+          descripton={
+            'correct personal information is essential part. correctly fill you personal information'
+          }
+        ></Card>
+
+        {/* <div className="card">card</div>
+        <div className="card">card</div>
+        <div className="card">card </div>
+        <div className="card">card </div>
+        <div className="card">card</div>
+        <div className="card">card</div>
+        <div className="card">card </div>
+        <div className="card">card</div>
+        <div className="card">card</div> */}
       </div>
     </div>
   );
-}
-
-function SideBar({ children }) {
-  return <div className="side-bar">class2 </div>;
 }
 function CvContainer({ children }) {
   return <div className="cv-container">class3 </div>;
 }
 function Footer({ children }) {
   return <div className="footer">footer </div>;
+}
+
+function Card({ tittle, descripton, children }) {
+  return (
+    <div className="card">
+      <h3>{tittle}</h3>
+      <p>{descripton}</p>
+      <div>{children}</div>
+      <button>
+        <span>⏬</span>
+      </button>
+    </div>
+  );
 }
